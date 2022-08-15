d2e46079e5e1908de5d01789c1de6a7f441369cb
marctysonclebert <codewithmarc@gmail.com>
2022-08-15 14:13:45 -0400

    Initial Commit

--- controllers/UserControllers.js
+++ controllers/UserControllers.js
@@ -1,13 +1,38 @@
-const addUser = (req, res) => {
-  res.status(201).json({});
+const User = require("../models/UserModel");
+
+const addUser = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    const user = await User.create({ username, email, password });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+  //   res.status(200).json(req.body);
 };
 
-const getUsers = (req, res) => {
-  res.status(200).json([]);
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 };
 
-const getUser = (req, res) => {
-  res.status(200).json({});
+const getUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findOne({ _id: id });
+
+    if (!user) {
+      res.status(404).json(`User with id: ${id} not found...`);
+    } else {
+      res.status(200).json(user);
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
 };
 
 module.exports = { addUser, getUsers, getUser };
--- index.js
+++ index.js
@@ -1,11 +1,25 @@
+require("dotenv/config");
+
 const express = require("express");
+const mongoose = require("mongoose");
 
 const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use("/users", require("./routes/UserRouter"));
 
-app.listen(port, () => {
-  console.log(`Server is running http://localhost:${port}`);
+mongoose.connect(process.env.DATABASE_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
+
+mongoose.connection
+  .on("error", () => console.log(`Error connecting to the database...`))
+  .once("open", () => {
+    app.listen(port, () => {
+      console.log(`Server is running http://localhost:${port}`);
+    });
+  });
