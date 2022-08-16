6f302ed17485b37032c06046b8ca1e06a79d2e7e
marctysonclebert <codewithmarc@gmail.com>
2022-08-16 12:16:43 -0400

    Initial Commit

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
 
 const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/users", require("./routes/UserRouter"));
